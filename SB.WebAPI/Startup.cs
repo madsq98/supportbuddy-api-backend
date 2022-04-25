dfec85a24407195b1c85741d64d28c08374d43bc
madsq98-desktop <mads.q@outlook.com>
2022-04-25 15:41:06 +0200

    Update Startup.cs

--- SB.WebAPI/Startup.cs
+++ SB.WebAPI/Startup.cs
@@ -110,7 +110,7 @@ namespace SB.WebAPI
             services.AddScoped<I_RW_Repository<Answer>, AnswerRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline..
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
