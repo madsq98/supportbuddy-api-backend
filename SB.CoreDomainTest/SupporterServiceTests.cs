4446f96c7a79ac07615cc2ba4023d16d3ea294c6
LindholmGithub <christianlindholm@jubii.dk>
2022-05-24 15:41:45 +0200

    Added final Tests for Tickets and Supporters

---
+++ SB.CoreDomainTest/SupporterServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Core.Models;
+using Moq;
+using NUnit.Framework;
+using SB.Domain.IRepositories;
+using SB.Domain.Services;
+
+namespace SB.CoreDomainTest
+{
+    [TestFixture]
+    public class SupporterServiceTests
+    {
+        [Test]
+        public void SupporterService_ThrowsInvalidDataException()
+        {
+            var mockRepoSupporters = new Mock<I_RW_Repository<Supporter>>();
+            var supporterService = new SupporterService(mockRepoSupporters.Object);
+            var supporter = new Supporter()
+            {
+                Username = "",
+                Password = "",
+                UserInfo = new UserInfo
+                {
+                    Email = "maildotcom",
+                    FirstName = "",
+                    LastName = "",
+                    PhoneNumber = 1000000000
+                }
+            };
+            Assert.Throws<InvalidDataException>(() => supporterService.Store(supporter));
+        }
+
+        [Test]
+        public void SupporterService_ThrowsInvalidDataException_PhoneNumberError_WithMessage()
+        {
+            var mockRepoSupporters = new Mock<I_RW_Repository<Supporter>>();
+            var supporterService = new SupporterService(mockRepoSupporters.Object);
+            var supporter = new Supporter()
+            {
+                Username = "Test",
+                Password = "test123",
+                UserInfo = new UserInfo
+                {
+                    Email = "test@test.com",
+                    FirstName = "Test",
+                    LastName = "Testersen",
+                    PhoneNumber = 1000000000
+                }
+            };
+            var exception = Assert.Throws<InvalidDataException>(() => supporterService.Store(supporter));
+            Assert.AreEqual("Invalid data supplied. Phone number length must be over zero and under nine.",
+                exception.Message);
+        }
+    }
+}
---
+++ SB.CoreDomainTest/TicketServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using Core.IServices;
+using Core.Models;
+using Moq;
+using NUnit.Framework;
+using SB.Domain.IRepositories;
+using SB.Domain.Services;
+using SB.EFCore.Repositories;
+
+namespace SB.CoreDomainTest
+{
+    [TestFixture]
+    public class TicketServiceTests
+    {
+        [Test]
+        public void TicketService_ThrowsInvalidDataException()
+        {
+            var mockRepoTickets = new Mock<I_RW_Repository<Ticket>>();
+            var mockRepoAnswers = new Mock<I_RW_Repository<Answer>>();
+            var ticketService = new TicketService(mockRepoTickets.Object,mockRepoAnswers.Object);
+            var ticket = new Ticket
+            {
+                Message = "",
+                Subject = "",
+                UserInfo = new UserInfo
+                {
+                    Email = "maildotcom",
+                    FirstName = "",
+                    LastName = "",
+                    PhoneNumber = 1000000000
+                }
+            };
+            Assert.Throws<InvalidDataException>(() => ticketService.Store(ticket));
+        }
+        [Test]
+        public void TicketService_ThrowsInvalidDataException_PhoneNumberError_WithMessage()
+        {
+            var mockRepoTickets = new Mock<I_RW_Repository<Ticket>>();
+            var mockRepoAnswers = new Mock<I_RW_Repository<Answer>>();
+            var ticketService = new TicketService(mockRepoTickets.Object,mockRepoAnswers.Object);
+            var ticket = new Ticket
+            {
+                Message = "Bobby",
+                Subject = "Bobsen",
+                UserInfo = new UserInfo
+                {
+                    Email = "test@test.com",
+                    FirstName = "Bobby",
+                    LastName = "Bobsen",
+                    PhoneNumber = 1000000000
+                }
+            };
+            var exception = Assert.Throws<InvalidDataException>(() => ticketService.Store(ticket));
+            Assert.AreEqual("Invalid data supplied. Phone number length must be over zero and under nine.",exception.Message);
+        }
+    }
+}
